e45f8ef2b2d75cec1585b66f3284644243d0932e
Jonathan Hong <jonhong777@gmail.com>
2018-08-05 16:02:56 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -53,7 +53,7 @@ class Dog
     Dog.new_from_db(row)
   end
 
-  def self.find_or_create_by(name, breed)
+  def self.find_or_create_by(name:, breed:)
     sql = <<-SQL
           SELECT * FROM dogs
           WHERE name = ? AND breed = ?
