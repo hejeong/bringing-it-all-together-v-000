b4834f252b4efad508c5ce4369e3f4b691c90cd2
Jonathan Hong <jonhong777@gmail.com>
2018-08-05 16:07:07 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -74,7 +74,7 @@ class Dog
     row = DB[:conn].execute(sql, name).first
     Dog.new_from_db(row)
   end
-  
+
   def self.new_from_db(row)
     Dog.new(name: row[1], breed: row[2], id: row[0])
   end
@@ -84,5 +84,6 @@ class Dog
           UPDATE dogs SET name = ?, breed = ?
           WHERE id = ?
           SQL
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 end
